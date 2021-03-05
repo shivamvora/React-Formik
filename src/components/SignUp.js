b3d73beb79a1e14727778bb054c4cac274f8da8e
shivamvora <shivampatel27102001@gmail.com>
2021-03-05 13:15:08 +0530

    formik

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,10 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Formik, Form } from 'formik';
+import TextField from './TextField';
 
 const SignUp = () => {
     return (
-        <div>
-            <h1>SignUp</h1>
-        </div>
+        <Formik initialValues={{ firstName: '', lastName: '', email: '', password: '', conFirmPassword: '' }}>
+            {formik => (
+                <div>
+                    <h1 className="my-4 font-weight-bold-display-4">
+                        Register
+                    </h1>
+                    {console.log( formik.values )}
+                    <Form>
+                        <TextField label="First Name" name="firstName" type="text" />
+                        <TextField label="Last Name" name="lastName" type="text" />
+                        <TextField label="Email" name="email" type="email" />
+                        <TextField label="Password" name="password" type="password" />
+                        <TextField label="Confirm Password" name="confirmPassword" type="password" />
+                        <button className="btn btn-dark mt-3" type="submit">Register</button>
+                        <button className="btn btn-danger mt-3 ml-3" type="reset">Reset</button>
+                    </Form>
+                </div>
+            )}
+        </Formik>
     )
 }
 
---
+++ src/components/TextField.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useField } from 'formik';
+
+const TextField = ( { label, ...props } ) => {
+    const [field, meta] = useField( props );
+    return (
+        <div className="mb-2">
+            <label htmlFor={field.name}>
+                {label}
+            </label>
+            <input type="text" className="form-control shadow-none" {...field} {...props} autoComplete="off" />
+        </div>
+    )
+}
+
+export default TextField;

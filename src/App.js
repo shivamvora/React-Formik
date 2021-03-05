875b0699529b04e023635009ab28f8cb396bc90d
shivamvora <shivampatel27102001@gmail.com>
2021-03-05 12:00:27 +0530

    formik add

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import rocketImg from './assets/rocket.png';
+import SignUp from './components/SignUp';
+
+const App = () => {
+    return (
+        <div className="container mt-3">
+            <div className="row">
+                <div className="col-md-">
+                    <SignUp />
+                </div>
+                <div className="col-md-7">
+                    <img className="img-fluid w-100" src={rocketImg} alt="check source" />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default App;
